ea648632b525ac1c767a7a3dfd9c27dbaa60f997
Tux <46397098+rtvua@users.noreply.github.com>
2021-09-21 09:50:30 +0300

    ...

---
+++ lab1/src/main/java/lab1/Person.java
@@ -0,0 +1,17 @@
+package lab1;
+
+public class Person {
+
+    private String name;
+    private String surname;
+    private String patronymic;
+
+    private int passportID;
+
+    public Person() {
+        name = "N/A";
+        surname = "N/A";
+        patronymic = "N/A";
+        passportID = 0;
+    }
+}
