5568533036e0414258f318c4ec7b2a1e9c1d47df
Beni from pc <benikuchuryan@gmail.com>
2022-01-27 17:53:52 +0200

    3rdlab

---
+++ lab3/src/main/java/lab3/AirportApp.java
@@ -0,0 +1,100 @@
+package lab3;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.partitioningBy;
+
+public class AirportApp {
+    public static List<Employee> employees = new ArrayList<Employee>();
+    public static List<Person> persons = new ArrayList<Person>();
+
+    List<Person> getByName(String name){
+        List<Person> newList = new ArrayList<Person>();
+        for (Person p:persons){
+            if(p.getName() == name) newList.add(p);
+        }
+        for (Employee e:employees){
+            if(e.getName() == name) newList.add(e);
+        }
+        return newList;
+    }
+
+    List<Employee> getByPosition(String position){
+        List<Employee> newList = new ArrayList<Employee>();
+        for (Employee e:employees){
+            if(e.getPosition() == position) newList.add(e);
+        }
+        return newList;
+    }
+
+    List<Employee> sortByPosition(){
+        List<Employee> newList = new ArrayList<Employee>();
+        List<String> posList = new ArrayList<String>();
+        String position = null;
+        for (Employee e:employees){
+            position = e.getPosition();
+            if(!posList.contains(position)) posList.add(position);
+        }
+        posList.sort(Comparator.naturalOrder());
+        for (String pos:posList){
+            for(Employee e:employees){
+                if(e.getPosition() == pos) newList.add(e);
+            }
+        }
+        return newList;
+    }
+
+
+    public static void main(String[] args) {
+        Person p1 = new Person.Builder().setName("Joe").setSurname("Parkinson").setPatronymic("Jacks").setPassportID(UUID.randomUUID()).build();
+        Person p2 = new Person.Builder().setName("Artur").setSurname("Washington").setPatronymic("John").setPassportID(UUID.randomUUID()).build();
+        Person p3 = new Person.Builder().setName("Roman").setSurname("Steel").setPatronymic("Andrew").setPassportID(UUID.randomUUID()).build();
+        Person p4 = new Person.Builder().setName("Alex").setSurname("Aaron").setPatronymic("John").setPassportID(UUID.randomUUID()).build();
+        Person p5 = new Person.Builder().setName("Artur").setSurname("Smith").setPatronymic("Joe").setPassportID(UUID.randomUUID()).build();
+
+
+
+        Employee e1 = new Employee.Builder().setPerson(p1).setPosition("Top-manager").setWorkerID(UUID.randomUUID()).build();
+        Employee e2 = new Employee.Builder().setPerson(p2).setPosition("Pilot").setWorkerID(UUID.randomUUID()).build();
+        Employee e3 = new Employee.Builder().setPerson(p3).setPosition("Manager").setWorkerID(UUID.randomUUID()).build();
+        Employee e4 = new Employee.Builder().setPerson(p4).setPosition("Air controller").setWorkerID(UUID.randomUUID()).build();
+        Employee e5 = new Employee.Builder().setPerson(p5).setPosition("Pilot").setWorkerID(UUID.randomUUID()).build();
+
+
+        AirportApp airport = new AirportApp();
+
+        airport.persons.add(p1);
+        airport.persons.add(p2);
+        airport.persons.add(p3);
+        airport.persons.add(p4);
+        airport.persons.add(p5);
+
+
+        airport.employees.add(e1);
+        airport.employees.add(e2);
+        airport.employees.add(e3);
+        airport.employees.add(e4);
+        airport.employees.add(e5);
+
+        System.out.println(airport.getByPosition("Pilot") + "\n");
+        System.out.println(airport.getByName("Artur") + "\n");
+        System.out.println("Sorting: " + airport.sortByPosition() + "\n");
+
+        Stream<Person> p = airport.persons.stream();
+        Stream<Employee> e = airport.employees.stream();
+
+
+
+        p.filter(x -> x.getName().length() < 5).forEach(System.out::println);
+        e.filter(x -> x.getName() == "Alex").forEach(System.out::println);
+
+
+        e = airport.employees.stream();
+        e.filter(x -> x.getPosition() == "Top-manager").forEach(System.out::println);
+
+
+
+    }
+}
---
+++ lab3/src/main/java/lab3/Person.java
@@ -0,0 +1,83 @@
+package lab3;
+
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * @toString
+ */
+
+public class Person {
+    protected String name;
+    protected String surname;
+    protected String patronymic;
+
+    protected UUID passportID;
+
+    protected Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                name + ", " +
+                surname + ',' +
+                patronymic + ',' +
+                passportID +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return passportID == person.passportID && name.equals(person.name) && surname.equals(person.surname) && patronymic.equals(person.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, patronymic, passportID);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+
+    public static class Builder {
+        private Person person;
+        public Builder(){
+            person = new Person();
+        }
+        public Builder setName(String name){
+            person.name = name;
+            return this;
+        }
+        public Builder setSurname(String surname){
+            person.surname = surname;
+            return this;
+        }
+        public Builder setPatronymic(String patronymic){
+            person.patronymic = patronymic;
+            return this;
+        }
+        public Builder setPassportID(UUID passportID){
+            person.passportID = passportID;
+            return this;
+        }
+        public Person build(){
+            if(person.name == null) throw new IllegalArgumentException("Name can't be null");
+            if(person.surname == null) throw new IllegalArgumentException("Surname can't be null");
+            if(person.patronymic == null) throw new IllegalArgumentException("Patronymic can't be null");
+            if(person.passportID == null) throw new IllegalArgumentException("Name can't be null");
+            return person;
+        }
+    }
+
+
+
+
+}
